dc652f08efd0f4a86de90280ac8f8bee6a35fac0
Artemqeqwe <artemgerasimov287@gmail.com>
2024-09-26 18:15:20 +0200

    Create 5.3.py

---
+++ 5.3.py
@@ -0,0 +1,9 @@
+import string
+text = input('Enter your text:')
+punc = str.maketrans('', '', string.punctuation )
+clean_text = text.translate(punc)
+clean_text= clean_text.title().replace(' ', '')
+if len(clean_text) > 140:
+    clean_text=clean_text[:140]
+print(f'#{clean_text}')
+print(len(clean_text))
